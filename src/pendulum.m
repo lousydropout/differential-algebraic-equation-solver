78af2a11caeb4342d2a0f7bc6183e1e23ff8dd67
vincent-chan <vincent.paktoo.chan@gmail.com>
2017-06-10 14:04:22 +0900

    changed file names

--- src/pendulum.m
+++ src/pendulum.m
@@ -1,18 +1,21 @@
 clc; clear; close;
 
-N = 50;     % no. collocation points
+N = 50;     % no. collocation points (RK4 uses 4 collocation
+            % points) per timestep
+
 t_start = 0; 
 t_finish = 10;
 t_step = 3.1;
+
 [op.D, op.t] = cheb(N-1, 0, t_step);  % set up grid & derivative op.
 
 
 %% parameters, held in scalar data structure 'param'
 param.N   = N;     % no. collocation points
 param.m   = 1;     % mass
-param.l   = 13.7;    % length
+param.l   = 13.7;  % length
 param.g   = 10;    % gravitational acceleration
-param.tol = 1.e-6;  % error tolerance
+param.tol = 1.e-6; % error tolerance
 
 
 
@@ -46,23 +49,23 @@ while (t < t_finish)
     
     %% set up vectors
     % used for DAE case
-    x = ic.x0 * ones(N, 1);
-    u = ic.u0 * ones(N, 1);
-    y = ic.y0 * ones(N, 1);
-    v = ic.v0 * ones(N, 1);
-    f = ic.f0 * ones(N, 1);
+    x = ic.x0 * ones (N, 1);
+    u = ic.u0 * ones (N, 1);
+    y = ic.y0 * ones (N, 1);
+    v = ic.v0 * ones (N, 1);
+    f = ic.f0 * ones (N, 1);
     w = [x; u; y; v; f];
 
 
     %% Solve DAE
-    [u, success] = solve(@func_dae, @jacobian_dae, w, op, param, ic);
+    [u, success] = solve(@func, @jacobian, w, op, param, ic);
     if (success == true)
         w = u;
-        x = w(1:N);
-        u = w(  N + 1: 2*N);
-        y = w(2*N + 1: 3*N);
-        v = w(3*N + 1: 4*N);
-        f = w(4*N + 1: 5*N);
+        x = w (1:N);
+        u = w (  N + 1: 2*N);
+        y = w (2*N + 1: 3*N);
+        v = w (3*N + 1: 4*N);
+        f = w (4*N + 1: 5*N);
     endif
 
     
@@ -82,4 +85,5 @@ while (t < t_finish)
 endwhile    
 
 %% Plot and compare y for ODE and DAE solutions
-hold off; plot(sol.t, sol.y); 
+hold off; 
+plot(sol.t, sol.y, 'o'); 
